2d2cefd365f6f1778f4980cbf8937c0c5d811c5b
Belle 168 <burula.termechikova@gmail.com>
2022-04-30 17:50:03 +0300

    fix the ui button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -5,29 +5,31 @@ const Button = (props) => {
 }
 
 const ButtonStyled = styled.button`
-   border: none;
+   border: 1px solid #dd8a08;
    width: ${({ width }) => width || '153px'};
    border-radius: 2px;
-   padding: 10px 16px;
-   background: #dd8a08;
+   padding: 8px 16px;
+   background: ${({ outline }) => (outline ? 'white' : '#dd8a08')};
    border-radius: 2px;
    font-family: 'Inter', sans-serif;
    font-weight: 500;
    font-size: ${({ size }) => size || '14px'};
    line-height: 17px;
    text-transform: uppercase;
-   color: #f7f7f7;
+   color: ${({ outline }) => (outline ? '#DD8A08' : '#F7F7F7')};
    transition: 0.2s;
    cursor: pointer;
    :hover {
       background: #bb7200;
-      :active {
-         background: #f2b75b;
-         :disabled {
-            background: #c4c4c4;
-            cursor: not-allowed;
-         }
-      }
+      color: #f7f7f7;
+   }
+   :active {
+      background: #f2b75b;
+      color: #f7f7f7;
+   }
+   :disabled {
+      background: #c4c4c4;
+      cursor: not-allowed;
    }
 `
 
