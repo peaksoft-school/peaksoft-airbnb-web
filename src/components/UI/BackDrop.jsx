8a6aba411da20633e5ed5883eaf6f098b442e42b
kaliloaida <kaliloaida@gmail.com>
2022-04-30 17:48:50 +0600

    created ui-modal

---
+++ src/components/UI/BackDrop.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import ReactDOM from 'react-dom'
+
+const BackDrop = (props) => {
+   return (
+      <>
+         {ReactDOM.createPortal(
+            <BackDropStyle {...props} />,
+            document.getElementById('backdrop-root')
+         )}
+      </>
+   )
+}
+
+const BackDropStyle = styled.div`
+   position: fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100vh;
+   z-index: 10;
+   background: rgba(0, 0, 0, 0.5);
+   animation: BACKDROP ease-in 0.4s;
+   @keyframes BACKDROP {
+      0% {
+         opacity: 0;
+      }
+      100% {
+         opacity: 1;
+      }
+   }
+`
+export default BackDrop
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,42 @@
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import BackDrop from './BackDrop'
+
+const ModalOverly = (props) => {
+   return <ModalStyle {...props}>{props.children}</ModalStyle>
+}
+const Modal = (props) => {
+   return (
+      <>
+         <BackDrop />
+         {ReactDOM.createPortal(
+            <ModalOverly {...props} />,
+            document.getElementById('modal-root')
+         )}
+      </>
+   )
+}
+
+const ModalStyle = styled.div`
+   padding: 20px 25px;
+   position: fixed;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   border-radius: 2px;
+   z-index: 10;
+   background: #ffffff;
+   box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+   animation: MODAL linear 0.4s;
+   @keyframes MODAL {
+      0% {
+         opacity: 0;
+         top: 40%;
+      }
+      100% {
+         opacity: 1;
+         top: 50%;
+      }
+   }
+`
+export default Modal
