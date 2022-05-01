397b8671e73ac2ef9d80c23d8d98d77dda7ddb18
ulukmyrza2001 <97427570+ulukmyrza2001@users.noreply.github.com>
2022-05-01 21:48:29 +0600

    fix the button

--- src/components/UI/buttons/Button.jsx
+++ src/components/UI/buttons/Button.jsx
@@ -1,33 +1,34 @@
 import styled from 'styled-components'
 
 const Button = (props) => {
-   return <ButtonStyled {...props}>Default{props.children}</ButtonStyled>
+   return <ButtonStyled {...props}>{props.children}</ButtonStyled>
 }
 
 const ButtonStyled = styled.button`
-   border: none;
+   border: ${({ outline }) => (outline ? '1px solid #dd8a08' : 'none')};
    width: ${({ width }) => width || '153px'};
    border-radius: 2px;
    padding: 10px 16px;
-   background: #dd8a08;
+   background: ${({ outline }) => (outline ? 'white' : '#dd8a08')};
    border-radius: 2px;
    font-family: 'Inter', sans-serif;
-   font-weight: 500;
    font-size: ${({ size }) => size || '14px'};
-   line-height: 17px;
    text-transform: uppercase;
-   color: #f7f7f7;
+   color: ${({ outline }) => (outline ? '#DD8A08' : '#F7F7F7')};
    transition: 0.2s;
    cursor: pointer;
    :hover {
       background: #bb7200;
-      :active {
-         background: #f2b75b;
-         :disabled {
-            background: #c4c4c4;
-            cursor: not-allowed;
-         }
-      }
+      border: #bb7200;
+      color: #f7f7f7;
+   }
+   :active {
+      background: #f2b75b;
+      color: #f7f7f7;
+   }
+   :disabled {
+      background: #c4c4c4;
+      cursor: not-allowed;
    }
 `
 
