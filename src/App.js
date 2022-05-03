788dc8fef6b5c9baed2ae45cb1ed2e665c4371da
Belle 168 <burula.termechikova@gmail.com>
2022-05-03 11:40:58 +0300

    cancel-button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import CancelButton from './components/UI/buttons/CancelButton'
+
 function App() {
-   return <div className="App" />
+   return (
+      <div className="App">
+         <CancelButton />
+      </div>
+   )
 }
 
 export default App
