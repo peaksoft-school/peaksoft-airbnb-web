2653bc38317695dc20c116b58cb3f5ed43592167
ulukmyrza2001 <97427570+ulukmyrza2001@users.noreply.github.com>
2022-05-05 15:21:45 +0600

    imagePicker finish

---
+++ src/components/UI/image-picker/ImagePicker.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
+import styled from 'styled-components'
+import { ReactComponent as IconImagePicker } from '../../../assets/icons/imagepicker.svg'
+import Flex from '../ui-for-positions/Flex'
+import Text from '../typography/Text'
+import Title from '../typography/Title'
+
+function ImagePicker({ onDrop, file }) {
+   const { getRootProps, getInputProps } = useDropzone({
+      onDrop,
+      accept: 'image/*',
+   })
+   return (
+      <Flex align="center" gap="16px">
+         <Flex gap="5px" wrap="wrap">
+            {file.map((img) => (
+               <Image key={img} src={img} />
+            ))}
+            {file.length < 4 && (
+               <ImagePickerStyled {...getRootProps()}>
+                  <input {...getInputProps()} type="file" />
+                  <IconImagePicker />
+               </ImagePickerStyled>
+            )}
+         </Flex>
+         {file.length === 0 && (
+            <Flex direction="column">
+               <Title color="#266BD3">Add photos to the review</Title>
+               <Text>
+                  it will become more noticeable and even more useful. You can
+                  upload up to 4 potos
+               </Text>
+            </Flex>
+         )}
+      </Flex>
+   )
+}
+const Image = styled.img`
+   width: 100px;
+   height: 100px;
+   @media (max-width: 425px) {
+      width: 60px;
+      height: 60px;
+   }
+`
+const ImagePickerStyled = styled.div`
+   width: 100px;
+   height: 100px;
+   background: #f3f3f3;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   cursor: pointer;
+   :hover {
+      background: #f3f3f3;
+      border: 1px solid #c4c4c4;
+   }
+   @media (max-width: 425px) {
+      width: 60px;
+      height: 60px;
+   }
+`
+export default ImagePicker
