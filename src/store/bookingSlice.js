8239a69218f739d6126ea900e75d4ebf73d01b54
ulukmyrza2001 <97427570+ulukmyrza2001@users.noreply.github.com>
2022-05-19 03:27:34 +0600

    regions

--- src/store/bookingSlice.js
+++ src/store/bookingSlice.js
@@ -1,13 +1,35 @@
-const { createSlice } = require('@reduxjs/toolkit')
+import { fetchApi } from '../api/fetchApi'
+
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit')
+
+export const getRegions = createAsyncThunk(
+   'region/getRegions',
+   async (_, { rejectWithValue }) => {
+      try {
+         return fetchApi({
+            path: 'api/regions',
+            method: 'GET',
+         })
+      } catch (error) {
+         rejectWithValue(error.message)
+      }
+   }
+)
 
 const initState = {
    booking: [],
+   regions: [],
 }
 
 const bookingSlice = createSlice({
    name: 'booking',
    initialState: initState,
    reducers: {},
+   extraReducers: {
+      [getRegions.fulfilled]: (state, { payload }) => {
+         state.regions = payload.data
+      },
+   },
 })
 
 export const bookingActions = bookingSlice.actions
