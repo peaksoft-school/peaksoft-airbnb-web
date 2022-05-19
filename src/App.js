461709a416875795007c920c81543916de411cf4
Belle 168 <burula.termechikova@gmail.com>
2022-05-19 16:20:28 +0300

    create input search

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AdminRoutes from './routes/AdminRoutes'
 import UserRoutes from './routes/UserRoutes'
 import { KEY_AUTH } from './utils/constants/general'
 import { saveToLocalStorage } from './utils/helpers/general'
+import InputSearch from './components/search-bar'
 
 function App() {
    const { user, token, isAuthorized, role } = useSelector(
@@ -16,6 +17,7 @@ function App() {
       <div>
          <UserRoutes />
          <AdminRoutes />
+         <InputSearch />
       </div>
    )
 }
---
+++ src/components/search-bar/index.jsx
@@ -0,0 +1,90 @@
+import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import Flex from '../UI/ui-for-positions/Flex'
+import { ReactComponent as SearchIcon } from '../../assets/icons/search.svg'
+import media from '../../utils/helpers/media'
+import frame from '../../assets/icons/Frame.svg'
+import { fetchApi } from '../../api/fetchApi'
+
+const InputSearch = () => {
+   const [regions, setRegions] = useState([])
+   const [search, setSearch] = useState('')
+   const { pathname } = useLocation()
+
+   const getRegions = () => {
+      fetchApi()
+   }
+   useEffect(() => {
+      getRegions()
+   }, [])
+
+   const filteredRegions = regions.filter((regions) => {
+      return regions.name.toLowerCase().includes(search.toLocaleLowerCase())
+   })
+
+   if (pathname === '/main') {
+      return (
+         <DivInput>
+            <img src={frame} alt="find" />
+            <Input
+               placeholder="Region, city , apartment, house..."
+               onChange={(event) => setSearch(event.target.value)}
+            />
+         </DivInput>
+      )
+   }
+   return (
+      <FlexSearch justify="end" align="center">
+         <SearchIconWrapper>
+            <SearchIcon />
+         </SearchIconWrapper>
+         <Search
+            placeholder="search"
+            onChange={(event) => setSearch(event.target.value)}
+         />
+         {filteredRegions.map()}
+      </FlexSearch>
+   )
+}
+
+const SearchIconWrapper = styled.div`
+   transform: translate(40px, 2px);
+`
+
+const FlexSearch = styled(Flex)`
+   width: 100%;
+   ${media.tablet`
+   width:300rem;
+   `}
+`
+
+const DivInput = styled.div`
+   max-width: 725px;
+   width: 100%;
+   height: 42px;
+   margin: 0 auto;
+   background-color: white;
+   display: flex;
+   align-items: center;
+   margin-top: 100px;
+   & img {
+      width: 18.62px;
+      height: 18.62px;
+      margin: 0 15px 0 15px;
+      color: #7d7d7d;
+   }
+`
+const Input = styled.input`
+   width: 100%;
+   height: 34px;
+   border: none;
+   font-size: 17px;
+   outline: none;
+`
+const Search = styled(Input)`
+   width: 100%;
+   margin: 0 10px 0 0;
+   padding-left: 45px;
+`
+export default InputSearch
