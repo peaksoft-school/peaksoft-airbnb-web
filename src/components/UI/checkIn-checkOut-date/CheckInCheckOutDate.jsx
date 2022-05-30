abab3e8e8de9dddf2d553047523111f124ee5689
Belle 168 <burula.termechikova@gmail.com>
2022-05-30 14:40:44 +0300

    finish check in date

--- src/components/UI/checkIn-checkOut-date/CheckInCheckOutDate.jsx
+++ src/components/UI/checkIn-checkOut-date/CheckInCheckOutDate.jsx
@@ -5,32 +5,34 @@ import Title from '../typography/Title'
 import Flex from '../ui-for-positions/Flex'
 import { Avatar } from '@mui/material'
 
-const CheckInCheckOutDate = ({ price, date }) => {
+const CheckInCheckOutDate = ({ price, date, name, email }) => {
    return (
       <Wrapper>
          <Flex direction="column" align="center" width="100%">
             <Flex gap="3px">
-               <Title size="20px">$26{price} /</Title>
+               <Title size="20px">{price} /</Title>
                <Text size="18px">day</Text>
             </Flex>
             <Flex width="100%" margin="10px 0 20px 0 ">
                <hr width="100%" color="#d3d3d3" />
             </Flex>
-            <Flex margin="4px 0px 14px 0" gap="10rem">
-               <Text className="checkIn">Check in</Text>
-               <Text>Check out</Text>
-            </Flex>
-            <Flex gap="10rem" margin="0 0 16px 0">
-               <Title>22.09.2022{date}</Title>
-               <Title>30.09.2022{date}</Title>
+            <Flex width="100%" justify="space-between">
+               <Flex direction="column" gap="1rem">
+                  <Text className="checkIn">Check in</Text>
+                  <Title size="14px">{date}</Title>
+               </Flex>
+               <Flex direction="column" gap="1rem">
+                  <Text>Check out</Text>
+                  <Title size="14px">{date}</Title>
+               </Flex>
             </Flex>
             <Flex margin="32px 0 0 0 ">
                <AvatarStyle>
                   <Avatar />
                </AvatarStyle>
                <UserStyle>
-                  <Title>Anna Annova</Title>
-                  <Text>anna@gmail.com</Text>
+                  <Title>{name}</Title>
+                  <Text>{email}</Text>
                </UserStyle>
             </Flex>
          </Flex>
@@ -39,14 +41,15 @@ const CheckInCheckOutDate = ({ price, date }) => {
 }
 
 const Wrapper = styled.div`
-   width: 400px;
+   max-width: 400px;
+   width: 100%;
    height: 157px;
    background-color: #ffffff;
-   padding-top: 20px;
+   padding: 20px;
    .checkIn {
       padding-right: 30px;
    }
-   @media (max-width: 470px) {
+   @media (max-width: 370px) {
       gap: 0px;
       padding: 0 6px 6px 6px;
    }
